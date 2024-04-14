090d5f59fa99d6dee31fae41b6d06c9ce331483c
Jyupi <86870091+Jyupi@users.noreply.github.com>
2024-04-15 03:06:00 +0900

    Add files via upload

---
+++ weather2.js
@@ -0,0 +1,28 @@
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input").value;
+  fetchWeather(cityInput);
+}
+
+function fetchWeather(cityName) {
+  let apiKey = "81d91288fe6a7e943f3251058e6c1276";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      updateWeather(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+    });
+}
+
+function updateWeather(data) {
+  document.querySelector("#cityname").innerHTML = data.name;
+  document.querySelector(".emojitemp").innerHTML = `🌡️ ${Math.round(
+    data.main.temp - 273.15
+  )}°C`;
+}
+
+document.querySelector("#search-form").addEventListener("submit", search);
